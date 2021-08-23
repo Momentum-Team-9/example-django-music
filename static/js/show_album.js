b64f9d2f34942b0c37e4a4c9d2afae82314e1bcb
Amy Gori <amydgori@gmail.com>
2021-08-23 19:34:33 -0400

    Update the DOM with favorited data

--- static/js/show_album.js
+++ static/js/show_album.js
@@ -1,18 +1,22 @@
-console.log("HELLOOOO HI HEY")
+console.log('HELLOOOO HI HEY')
 
-const favLink = document.querySelector(".fav-link")
+const favLink = document.querySelector('.fav-link')
 
-favLink.addEventListener("click", (event) => {
+favLink.addEventListener('click', (event) => {
   event.preventDefault()
   const url = event.target.parentNode.href
   const heartIcon = event.target
   fetch(url, {
-    headers: {'X-Requested-With': 'XMLHttpRequest'},
+    headers: { 'X-Requested-With': 'XMLHttpRequest' },
   })
-  .then(res => res.json())
-  .then(data => {
-    // here I can do stuff with that data!
-    console.log(data)
-  })
+    .then((res) => res.json())
+    .then((data) => {
+      // here I can do stuff with that data!
+      console.log(data)
+      if (data['favorited']) {
+        heartIcon.classList.replace('far', 'fas')
+      } else {
+        heartIcon.classList.replace('fas', 'far')
+      }
+    })
 })
-
